ebd7df68ea6cd5e5c567e24d67f5a35ba5e7cd67
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-01-16 10:25:58 -0600

    don't need to save png images any more

--- Main_Module.py
+++ Main_Module.py
@@ -10,7 +10,6 @@ import scipy.misc as scm
 
 def write_image(base_path,image_file,out_path, no_rotations, no_flip_lr, images_description='LiTs'):
     print(image_file)
-    path = 'C:\\users\\bmanderson\\desktop\\images_LiTs\\'
     index = int((image_file.split('volume-')[1]).split('.nii')[0])
     if os.path.exists(os.path.join(out_path, 'Overall_mask_LiTs_y' + str(index))):
         return None
@@ -30,13 +29,16 @@ def write_image(base_path,image_file,out_path, no_rotations, no_flip_lr, images_
     if index not in no_flip_lr:
         img = img[:, :, ::-1]
         label = label[:,:,::-1]
-    locations = np.where(np.max(label,axis=(1,2)) != 0)[0]
-    i = locations[len(locations)//2]
-    out = np.zeros([512, 512 * 2])
-    out[:, 0:512] = img[i]
-    max_val = img[i].max()
-    out[:, 512:] = label[i] * max_val
-    scm.imsave(os.path.join(path, 'combined_' + str(index) + '.png'), out)
+    write_png_images = False
+    if write_png_images:
+        path = r'C:\Users\bmanderson\Desktop\images_LiTs'
+        locations = np.where(np.max(label,axis=(1,2)) != 0)[0]
+        i = locations[len(locations)//2]
+        out = np.zeros([512, 512 * 2])
+        out[:, 0:512] = img[i]
+        max_val = img[i].max()
+        out[:, 512:] = label[i] * max_val
+        scm.imsave(os.path.join(path, 'combined_' + str(index) + '.png'), out)
     out_image_handle = sitk.GetImageFromArray(img)
     out_image_handle.SetSpacing(image_handle.GetSpacing())
     out_image_handle.SetOrigin(image_handle.GetOrigin())
@@ -68,11 +70,14 @@ def create_NIFTI_images(data_path,out_path, images_description='LiTs'):
         index = file.split('volume-')[1][:-4]
         # if os.path.exists(r'C:\Users\bmanderson\Desktop\images_LiTs\combined_{}.png'.format(index)):
         #     continue
-        write_image(data_path,file,out_path, no_rotations,no_flip_lr)
+        image_path = os.path.join(out_path, 'Overall_Data_' + images_description + '_' + str(index) + '.nii.gz')
+        if os.path.exists(image_path):
+            continue
+        write_image(data_path,file,out_path, no_rotations,no_flip_lr, images_description=images_description)
 
 
 def main():
     data_path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Fuentes_Data\LiTs\Images'
     out_path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Liver_Disease_Ablation_Segmentation\Niftii_Data'
     images_desc = 'LiTs'
-    create_NIFTI_images(data_path, out_path)
+    create_NIFTI_images(data_path, out_path, images_desc)
