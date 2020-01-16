aed6b7f870cb9605469cc831a00cfe97f3ea1048
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-01-16 15:22:35 -0600

    testing making_single_images

--- Main_Module.py
+++ Main_Module.py
@@ -3,6 +3,7 @@ __author__ = 'Brian M Anderson'
 
 from Plot_And_Scroll_Images.Plot_Scroll_Images import plot_scroll_Image
 from LiTs_Into_Niftii import create_NIFTI_images
+from Make_Single_Images.Make_Single_Images_Class import run_main
 
 
 def run_LiTs_to_NIFTII():
@@ -12,10 +13,18 @@ def run_LiTs_to_NIFTII():
     create_NIFTI_images(data_path, out_path, images_desc)
 
 def main():
-    create_niftii_images = True
+    create_niftii_images = False
     if create_niftii_images:
         run_LiTs_to_NIFTII()
-    xxx = 1
+    make_single_images = True
+    if make_single_images:
+        path = r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver\Liver_Disease_Ablation_Segmentation\Niftii_Data'
+        desired_output_spacing = (0.89648, 0.89648, 3)
+        extension = 16
+        write_images = True
+        re_write_pickle = False
+        thread_count = 1
+        run_main(path,desired_output_spacing,extension, write_images, re_write_pickle, thread_count)
 
 if __name__ == '__main__':
     main()
