fae83da383360f179a0cf31957cb9535425d6037
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-01-18 10:58:45 -0600

    added plotting

---
+++ Deep_Learning/Plot_Best_LR.py
@@ -0,0 +1,17 @@
+from Base_Deeplearning_Code.Finding_Optimization_Parameters.LR_Finder import make_plot, os
+
+out_path = os.path.join('..','..','Learning_Rates_Liver_Disease_GTV')
+all_lrs, all_metrics = [], []
+path = out_path
+for _, folders, _ in os.walk(path):
+    break
+for i in folders:
+    paths = os.path.join(path, i)
+    iterations = []
+    for _, iterations, _ in os.walk(paths):
+        break
+    if iterations:
+        paths = [os.path.join(path,i,iii) for iii in iterations]
+    print(i)
+    make_plot(paths,metric_list=['loss'],title=i, save_path=os.path.join(out_path), plot=True, auto_rates=True)
+
