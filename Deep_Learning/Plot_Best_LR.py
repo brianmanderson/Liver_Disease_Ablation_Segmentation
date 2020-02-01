4ca8ce74d8bcd5a4badabe957ebe08a82624d567
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-02-01 16:47:18 -0600

    update plotting for LR, added atrous_rate as lambda function

--- Deep_Learning/Plot_Best_LR.py
+++ Deep_Learning/Plot_Best_LR.py
@@ -1,20 +1,28 @@
 from Base_Deeplearning_Code.Finding_Optimization_Parameters.LR_Finder import make_plot, os
 
-out_path = os.path.join('..','..','Learning_Rates_Liver_Disease_GTV','Learning_Rates_Liver_Disease_GTV_weighted500_inverted')
-all_lrs, all_metrics = [], []
-path = out_path
-for _, folders, _ in os.walk(path):
-    break
-for i in folders:
-    paths = os.path.join(path, i)
-    iterations = []
-    for _, iterations, _ in os.walk(paths):
-        break
-    if iterations:
-        paths = [os.path.join(path,i,iii) for iii in iterations]
-    print(i)
-    try:
-        make_plot(paths,metric_list=['loss'],title=i, save_path=os.path.join(out_path), plot=True, auto_rates=True)
 
-    except:
-        continue
+def down_folder(input_path, base_input_path=''):
+    folders = []
+    for _, folders, _ in os.walk(input_path):
+        break
+    iterations_found = False
+    for folder in folders:
+        if folder.find('Iteration') == -1:
+            down_folder(os.path.join(input_path, folder),base_input_path=base_input_path)
+        else:
+            iterations_found = True
+            break
+    if iterations_found:
+        paths = [os.path.join(input_path, i) for i in folders if i.find('Iteration') != -1]
+        try:
+            print(input_path)
+            desc = ''.join(input_path.split(base_input_path)[-1].split('\\'))
+            save_path = os.path.join(base_input_path,'Outputs')
+            make_plot(paths, metric_list=['loss'], title=desc, save_path=save_path, plot=True,
+                      auto_rates=True, beta=0.98)
+        except:
+            xxx = 1
+    return None
+
+out_path = os.path.join('..','..','Learning_Rates_Liver_Disease')
+down_folder(out_path,base_input_path=out_path)
