78059458bf54dc94380567f4c4ce8f2540feadd1
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-04-01 09:22:12 -0500

    ready to run

--- Deep_Learning/Return_Train_Validation_Generators.py
+++ Deep_Learning/Return_Train_Validation_Generators.py
@@ -238,7 +238,7 @@ def return_generators(get_mean_std=False, liver_norm=True,num_patients=1,
     validation_generator = Data_Generator_Class(by_patient=True,num_patients=1, whole_patient=True, shuffle=False,
                                                 data_paths=paths_validation_generator, wanted_indexes=[1],expansion=expansion,
                                                 image_processors=image_processors_test)
-    while True:
+    while False:
         for i in range(1,len(train_generator)):
             print(i)
             x, y = train_generator.__getitem__(i)
@@ -280,5 +280,5 @@ def return_generators(get_mean_std=False, liver_norm=True,num_patients=1,
 
 
 if __name__ == '__main__':
-    return_generators(False, path_extension='Single_Images3D_None', cube_size=(8, 20, 120, 120), return_test=False)
+    # return_generators(False, path_extension='Single_Images3D_None', cube_size=(8, 20, 120, 120), return_test=False)
     pass
