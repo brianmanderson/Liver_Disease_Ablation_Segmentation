e7950c007ff4f08eba17ead693bfd856f4f3003c
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-04-01 07:40:22 -0500

    added a checking sphericity, volume, and number of voxels

---
+++ Deep_Learning/Check_Volume_Min_Patient.py
@@ -0,0 +1,28 @@
+__author__ = 'Brian M Anderson'
+# Created on 3/31/2020
+
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+path = os.path.join('.','Sphericity.xlsx')
+
+df = pd.read_excel(path)
+df = df.to_dict()
+
+out_dict = {}
+for index in range(len(df['Volume_cm3'])):
+    pat_id = df['Patient_ID'][index]
+    volume = df['Voxels'][index]
+    if pat_id in out_dict:
+        out_dict[pat_id] = max([volume,out_dict[pat_id]])
+    else:
+        out_dict[pat_id] = volume
+total_volume = []
+pat_keys = np.asarray(list(out_dict.keys()))
+for pat_id in pat_keys:
+    total_volume.append(out_dict[pat_id])
+total_volume = np.asarray(total_volume)
+print(np.min(total_volume))
