f467b2babafc28f2ed4dbd3283c61f39b0ea684b
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-05-04 15:49:11 -0500

    test to see if we have gpu on rc4

--- Deep_Learning/Optimization/Find_Best_LR_TF2.py
+++ Deep_Learning/Optimization/Find_Best_LR_TF2.py
@@ -18,7 +18,7 @@ def find_best_lr(optimizer='SGD', batch_size=16, path_desc='', bn_before_activat
         min_lr = 1e-10
         max_lr = 1
     hp_dict = return_hyper_parameters()
-    for iteration in [0]:
+    for iteration in [1]:
         for layer in hp_dict['layers'].domain.values:
             for filters in hp_dict['filters'].domain.values:
                 for max_filters in hp_dict['max_filters'].domain.values:
