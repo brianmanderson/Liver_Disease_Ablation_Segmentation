bfcd79c774392285ea767c6aa5201ada5c1102d8
bmanderson@mdanderson.org <bmanderson@mdanderson.org>
2020-05-04 12:56:59 -0500

    run with Adam opt

--- Deep_Learning/Optimization/Find_Best_LR_TF2.py
+++ Deep_Learning/Optimization/Find_Best_LR_TF2.py
@@ -4,7 +4,7 @@ from Base_Deeplearning_Code.Data_Generators.Return_Paths import *
 import tensorflow as tf
 from Base_Deeplearning_Code.Plot_And_Scroll_Images.Plot_Scroll_Images import plot_scroll_Image
 from Base_Deeplearning_Code.Finding_Optimization_Parameters.LR_Finder import LearningRateFinder
-from Return_Train_Validation_Generators_TF2 import return_generators, return_base_dict, get_layers_dict, return_things, return_hyper_parameters
+from Return_Train_Validation_Generators_TF2 import return_generators, return_base_dict, get_layers_dict, return_hyper_parameters
 from Base_Deeplearning_Code.Callbacks.TF2_Callbacks import SparseCategoricalMeanDSC
 from Base_Deeplearning_Code.Models.TF_Keras_Models import my_UNet
 
@@ -14,7 +14,7 @@ def find_best_lr(optimizer='SGD', batch_size=16, path_desc='', bn_before_activat
     min_lr = 1e-7
     max_lr = 1
     hp_dict = return_hyper_parameters()
-    for iteration in [0, 1, 2]:
+    for iteration in [0]:
         for layer in hp_dict['layers'].domain.values:
             for filters in hp_dict['filters'].domain.values:
                 for max_filters in hp_dict['max_filters'].domain.values:
@@ -22,9 +22,9 @@ def find_best_lr(optimizer='SGD', batch_size=16, path_desc='', bn_before_activat
                         batch_size=batch_size)
                     run_data = base_dict(min_lr=min_lr, max_lr=max_lr, filters=filters, max_filters=max_filters,
                                          layers=layer)
-                    run_data['Architecture']['bn_after_activation'] = not bn_before_activation
-                    layers_dict = get_layers_dict(**run_data['Architecture'], bn_before_activation=bn_before_activation)
-                    things = return_things(run_data, keys=['Architecture'])
+                    layers_dict = get_layers_dict(**run_data, bn_before_activation=bn_before_activation)
+                    things = ['layers_{}'.format(layer), 'filters_{}'.format(filters),
+                              'max_filters_{}'.format(max_filters), 'Optimizer_{}'.format(optimizer)]
                     things.append('{}_Iteration'.format(iteration))
                     out_path = os.path.join(morfeus_drive,path_desc,'Fully_Atrous')
                     for thing in things:
@@ -44,8 +44,12 @@ def find_best_lr(optimizer='SGD', batch_size=16, path_desc='', bn_before_activat
                     #           validation_data=validation_generator.data_set, validation_steps=5)
                     # data = next(iter(validation_generator.data_set))
                     # pred = model(data[0])
+                    if optimizer == 'SGD':
+                        lr_opt = tf.keras.optimizers.SGD
+                    else:
+                        lr_opt = tf.keras.optimizers.Adam
                     LearningRateFinder(epochs=10, model=model, metrics=['sparse_categorical_accuracy'],
-                                       out_path=out_path,
+                                       out_path=out_path, optimizer=lr_opt,
                                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
                                        steps_per_epoch=len(train_generator),
                                        train_generator=train_generator.data_set, lower_lr=1e-5, high_lr=1)
