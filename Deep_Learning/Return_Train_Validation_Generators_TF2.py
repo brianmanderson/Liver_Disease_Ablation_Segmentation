7e6dd7dea624b792bec25ebcc1e29bafe82ef55b
markba122@gmail.com <bmanderson@mdanderson.org>
2020-06-12 12:41:17 -0500

    makes sense to chop filters in half again as we go up, still increasing by growth-rate

--- Deep_Learning/Return_Train_Validation_Generators_TF2.py
+++ Deep_Learning/Return_Train_Validation_Generators_TF2.py
@@ -294,7 +294,7 @@ def get_layers_dict_dense_new(layers=1, filters=12, growth_rate=6, conv_lambda=0
     layers_dict['Base'] = encoding
     num_conv_blocks = num_conv_blocks_base
     for layer in layers_encoding[::-1]:
-        # filters = filters // 2
+        filters = filters // 2
         up_name = 'Layer_{}_Up'.format(layer)
         layers_dict['Layer_' + str(layer)]['Pooling']['Decoding'] = [
             lc.activation_layer('elu'), lc.batch_norm_layer(),
