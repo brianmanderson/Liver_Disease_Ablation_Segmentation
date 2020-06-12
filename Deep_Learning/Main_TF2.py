fbd8842328777363521d6698d6e345aef65be423
markba122@gmail.com <bmanderson@mdanderson.org>
2020-06-12 11:14:49 -0500

    run with latest model

--- Deep_Learning/Main_TF2.py
+++ Deep_Learning/Main_TF2.py
@@ -22,10 +22,10 @@ if find_lr:
     from Optimization.Find_Best_LR_TF2 import find_best_lr
     find_best_lr(optimizer='Adam', batch_size=batch_size, path_desc=path_desc, add=add)
 
-find_dense_lr_dense = True
+find_dense_lr_dense = False
 if find_dense_lr_dense:
     from Optimization.Find_Best_LR_TF2_Dense import find_best_lr
-    find_best_lr(batch_size=batch_size, path_desc=path_desc+'_test', add=add, cache_add=cache_add)
+    find_best_lr(batch_size=batch_size, path_desc=path_desc, add=add, cache_add=cache_add)
 '''
 Plot the LR, get the min and max from the images
 '''
@@ -41,7 +41,7 @@ if plot_lr:
 Now, we need to run the model for a number of epochs ~200, so we can get a nice curve to make final model
 decision based on
 '''
-run_200 = False
+run_200 = True
 if run_200:
     from Run_Model_TF2 import train_model
     run_best = True
--- Deep_Learning/Return_Train_Validation_Generators_TF2.py
+++ Deep_Learning/Return_Train_Validation_Generators_TF2.py
@@ -125,6 +125,12 @@ def return_dictionary_dense(base_dict, run_best=False):
             base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
                       min_lr=6e-7, max_lr=8e-3)
                       ]
+    dictionary = [
+        base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
+                  min_lr=1e-6, max_lr=1e-2),
+        base_dict(layers=3, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
+                  min_lr=6e-7, max_lr=8e-3)
+    ]
     return dictionary
 
 
