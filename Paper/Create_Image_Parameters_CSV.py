e5f54367e84a2adfca6c1043f945300fc156092b
Anderson <BMAnderson@mdanderson.org>
2020-07-27 14:36:26 -0500

    update main to write excel sheets

--- Paper/Create_Image_Parameters_CSV.py
+++ Paper/Create_Image_Parameters_CSV.py
@@ -2,26 +2,33 @@ __author__ = 'Brian M Anderson'
 # Created on 7/21/2020
 
 import os
+import pandas as pd
 import SimpleITK as sitk
 
 
 def identify_image_parameters(images_path=r'H:\Data\Local_Recurrence_Exports',
-                              out_path= r'K:\Morfeus\BMAnderson\Modular_Projects\Liver_Local_Recurrence_Work\Image_parameters.csv'):
+                              xlsx_path=r'K:\Morfeus\BMAnderson\Modular_Projects\Liver_Local_Recurrence_Work\Image_parameters.xlsx'):
     reader = sitk.ImageSeriesReader()
     reader.MetaDataDictionaryArrayUpdateOn()
     reader.LoadPrivateTagsOn()
-    fid = open(out_path, 'w+')
-    fid.write('MRN,Slice Thickness,Pixel_X,Pixel_Y\n')
+    out_dict = {'MRN':[], 'Slice Thickness': [], 'Pixel_X': [], 'Pixel_Y': []}
     for patient_path, dirs, files in os.walk(images_path):
-        files = [os.path.join(patient_path, i) for i in files if i.endswith('.dcm') and i.startswith('CT')]
+        files = [os.path.join(patient_path, i) for i in files if i.endswith('.dcm') and
+                 (i.startswith('CT') or i.startswith('image'))]
         if files:
+            print(patient_path)
             reader.SetFileNames(files[:2])
             dicom_handle = reader.Execute()
             slice_thickness = float(reader.GetMetaData(0, "0018|0050"))
             pixel_spacing = [float(i) for i in reader.GetMetaData(0, "0028|0030").split('\\')]
             MRN = reader.GetMetaData(0, "0010|0020")
-            fid.write('{},{},{},{}\n'.format(MRN, slice_thickness, pixel_spacing[0], pixel_spacing[1]))
-    fid.close()
+            out_dict['MRN'].append(MRN)
+            out_dict['Slice Thickness'].append(slice_thickness)
+            out_dict['Pixel_X'].append(pixel_spacing[0])
+            out_dict['Pixel_Y'].append(pixel_spacing[1])
+    df = pd.DataFrame(out_dict)
+    columns = ['MRN', 'Slice Thickness', 'Pixel_X', 'Pixel_Y']
+    df[columns].to_excel(xlsx_path, index=0)
     return None
 
 
--- Paper/Main.py
+++ Paper/Main.py
@@ -9,4 +9,8 @@ if combine_excel_sheets:
 
 find_image_parameters = True
 if find_image_parameters:
-    from Paper.Create_Image_Parameters_CSV import
+    from Paper.Create_Image_Parameters_CSV import identify_image_parameters
+    identify_image_parameters(images_path=r'K:\Morfeus\BMAnderson\CNN\Data\Data_Liver'
+                                          r'\Liver_Disease_Ablation_Segmentation\Nifti_to_dicom\LiTs',
+                              xlsx_path=r'K:\Morfeus\BMAnderson\Modular_Projects'
+                                        r'\Liver_Disease_Ablation_Segmentation_Work\Image_parameters_LiTs.xlsx')
