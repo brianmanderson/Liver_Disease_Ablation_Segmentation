337235436e7181169111df39696c102a630231b6
markba122@gmail.com <bmanderson@mdanderson.org>
2020-07-29 12:18:12 -0500

    testing with doubling filters

--- Deep_Learning/Return_Train_Validation_Generators_TF2.py
+++ Deep_Learning/Return_Train_Validation_Generators_TF2.py
@@ -263,7 +263,7 @@ def get_layers_dict_dense_new(layers=1, filters=12, growth_rate=6, conv_lambda=0
             names = names[:]
             filters += growth_rate
             filters = min([filters, max_filters])
-        filters /= 2
+        filters *= 2
         encoding_names.append(names)
         layers_dict['Layer_' + str(layer)]['Encoding'] += encoding
         previous_name = 'Layer_{}_Down'.format(layer)
@@ -313,7 +313,7 @@ def get_layers_dict_dense_new(layers=1, filters=12, growth_rate=6, conv_lambda=0
             filters += growth_rate
             filters = min([filters, max_filters])
         layers_dict['Layer_' + str(layer)]['Decoding'] = encoding
-        filters *= 2
+        filters /= 2
     final_steps = [lc.activation_layer('elu'), lc.batch_norm_layer(),
                    block(filters, kernel=(1, 1, 1), batch_norm=True, activation='elu'),
                    lc.convolution_layer(num_classes, batch_norm=False, activation='softmax')]
