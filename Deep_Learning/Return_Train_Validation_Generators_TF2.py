3a71886b6e2ed6f5da125c8ae0a5f977fd541cbf
markba122@gmail.com <bmanderson@mdanderson.org>
2020-07-29 11:13:29 -0500

    test with 16 filters

--- Deep_Learning/Return_Train_Validation_Generators_TF2.py
+++ Deep_Learning/Return_Train_Validation_Generators_TF2.py
@@ -126,10 +126,10 @@ def return_dictionary_dense(base_dict, run_best=False):
                       min_lr=6e-7, max_lr=8e-3)
                       ]
     dictionary = [
-        base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
+        base_dict(layers=2, max_conv_blocks=4, filters=16, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
                   min_lr=1e-6, max_lr=1e-3),
-        base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
-                  min_lr=1e-6, max_lr=1e-2)
+        # base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=4,
+        #           min_lr=1e-6, max_lr=1e-2)
     ]
     return dictionary
 
