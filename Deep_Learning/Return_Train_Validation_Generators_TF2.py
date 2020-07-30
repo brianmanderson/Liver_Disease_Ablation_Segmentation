696bdf2e6eea0ca40576c6573545bebb568488f7
markba122@gmail.com <bmanderson@mdanderson.org>
2020-07-29 21:40:27 -0500

    only the best model run now*

--- Deep_Learning/Return_Train_Validation_Generators_TF2.py
+++ Deep_Learning/Return_Train_Validation_Generators_TF2.py
@@ -126,14 +126,8 @@ def return_dictionary_dense(base_dict, run_best=False):
                       min_lr=6e-7, max_lr=8e-3)
                       ]
     dictionary = [
-        base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=0, growth_rate=0,
-                  min_lr=3e-6, max_lr=1e-2),
         base_dict(layers=2, max_conv_blocks=4, filters=8, num_conv_blocks=2, conv_lambda=1, growth_rate=0,
                   min_lr=2e-6, max_lr=2e-2),
-        base_dict(layers=2, max_conv_blocks=4, filters=16, num_conv_blocks=2, conv_lambda=0, growth_rate=0,
-                  min_lr=3e-6, max_lr=1e-2),
-        base_dict(layers=2, max_conv_blocks=4, filters=16, num_conv_blocks=2, conv_lambda=1, growth_rate=0,
-                  min_lr=2e-6, max_lr=1e-2)
     ]
     return dictionary
 
