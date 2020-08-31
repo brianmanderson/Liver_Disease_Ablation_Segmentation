303f3b59469c413837fc61c83b5bd81e30f81a6b
markba122@gmail.com <bmanderson@mdanderson.org>
2020-08-31 14:52:41 -0500

    commented, combines to create a dicom RT for liver and disease

--- French_3DCIR_Data/Create_Ground_Truth.py
+++ French_3DCIR_Data/Create_Ground_Truth.py
@@ -1,7 +1,7 @@
 __author__ = 'Brian M Anderson'
 # Created on 8/31/2020
 
-import os, pydicom
+import os, pydicom, shutil
 import SimpleITK as sitk
 from Dicom_RT_and_Images_to_Mask.Image_Array_And_Mask_From_Dicom_RT import Dicom_to_Imagestack
 from Dicom_RT_and_Images_to_Mask.Plot_And_Scroll_Images.Plot_Scroll_Images import plot_scroll_Image
@@ -34,26 +34,58 @@ def add_FrameOfReferenceUID_patid(path, patient_id):
     return None
 
 
-def main():
+def create_predictions():
+    prediction_path = r'L:\Clinical\Auto_Contour_Sites\Liver_Disease_Ablation_Auto_Contour\Input_3'
+
+
+def create_dicom_RT():
     path = r'C:\Users\bmanderson\Downloads\Unzipped\3Dircadb1'
     out_path = r'H:\Liver_Disease_Ablation\3Dircadb1\Fixed_Patients'
     for patient in os.listdir(path):
+        print(patient)
         patient_id = patient.split('.')[-1]
+        out_dir = os.path.join(out_path, 'Patient_{}'.format(patient_id))
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        elif os.path.exists(os.path.join(out_dir, 'Completed.txt')):
+            continue
         patient_path = os.path.join(path, patient)
         patient_dicom_path = os.path.join(patient_path, 'PATIENT_DICOM')
+        copy_files = [i for i in os.listdir(patient_dicom_path) if i.endswith('.dcm') and i not in os.listdir(out_dir)]
+        for file in copy_files:
+            shutil.copyfile(os.path.join(patient_dicom_path, file), os.path.join(out_dir, file))
         # add_dicom_tag(patient_dicom_path)
         add_FrameOfReferenceUID_patid(patient_dicom_path, patient_id)
-        image_reader = Dicom_to_Imagestack(get_images_mask=True)
-        image_reader.Make_Contour_From_directory(patient_dicom_path)
         for folder in os.listdir(os.path.join(patient_path, 'MASKS_DICOM')):
             print(folder)
             # add_dicom_tag(os.path.join(patient_path, 'MASKS_DICOM', folder))
             add_FrameOfReferenceUID_patid(os.path.join(patient_path, 'MASKS_DICOM', folder), patient_id)
+
+        '''
+        Now use the dicom readers to create the ground truth masks
+        '''
+        image_reader = Dicom_to_Imagestack(get_images_mask=True)
+        image_reader.Make_Contour_From_directory(patient_dicom_path)
+        background = np.zeros(image_reader.ArrayDicom.shape)
         liver_reader = Dicom_to_Imagestack(get_images_mask=True)
         liver_reader.Make_Contour_From_directory(os.path.join(patient_path, 'MASKS_DICOM', 'liver'))
         liver = liver_reader.ArrayDicom
-        liver = np.stack(liver, axis=-1)
+        liver_tumor_folders = [i for i in os.listdir(os.path.join(patient_path, 'MASKS_DICOM'))
+                               if i.startswith('livertumor')]
+        '''
+        Combine all tumors into 'Disease'
+        '''
+        tumor = np.zeros(liver.shape)
+        for tumor_folder in liver_tumor_folders:
+            tumor_reader = Dicom_to_Imagestack(get_images_mask=True)
+            tumor_reader.Make_Contour_From_directory(os.path.join(patient_path, 'MASKS_DICOM', tumor_folder))
+            tumor += tumor_reader.ArrayDicom
+        background = np.stack([background > 0, liver > 0, tumor > 0], axis=-1)
+        '''
+        Write out the RT structure
+        '''
+        image_reader.with_annotations(background, output_dir=out_dir, ROI_Names=['Liver', 'Disease'])
 
 
 if __name__ == '__main__':
-    main()
+    create_dicom_RT()
