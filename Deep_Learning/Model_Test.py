d9e2e69f29f9a3f3f2f41c32100251fe71b80e26
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-09 11:33:36 -0500

    test out everyting on DGX

--- Deep_Learning/Model_Test.py
+++ Deep_Learning/Model_Test.py
@@ -13,52 +13,39 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 os.environ['TF_GPU_THREAD_MODE'] = 'gpu_private'
 
 from Return_Train_Validation_Generators_TF2 import get_layers_dict_dense_less_decode, my_UNet, return_generators, get_layers_dict_dense_HNet, return_model, plot_scroll_Image, os
-# from Run_Model_TF2 import SparseCategoricalMeanDSC
+from Run_Model_TF2 import SparseCategoricalMeanDSC
 import numpy as np
+import SimpleITK as sitk
 # from tensorflow.keras.callbacks import TensorBoard
-# import tensorflow.keras as tfk
+import tensorflow.keras as tfk
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 # from HDenseUNet.denseunet import DenseUNet
 # from MyHybridDenseNet.Loading_Pretrained_DenseNet import DenseNet121
-
+from tensorflow.keras.applications.densenet import DenseNet121
 add = '_16'
 path_desc='TF_LR_2D_Dense_1mm_new'
-model_name = 'DenseNet'
+model_name = 'DenseNet2'
 cache_add = ''
 # x,y = next(iter(train_generator.data_set))
 # x1,y1 = next(iter(validation_generator.data_set))
-# path = r'H:\Liver_Disease_Ablation\Predictions_np'
-# images = [i for i in os.listdir(path) if i.startswith('Image')]
-# for file in images:
-#     x = np.load(os.path.join(path, file))
-#     pred = np.load(os.path.join(path, file.replace('Image', 'Prediction')))
-#     truth = np.load(os.path.join(path, file.replace('Image', 'Truth')))
-#     xxx = 1
-base_path, morfeus_drive, train_generator, validation_generator = return_generators(batch_size=0, add=add,cache_add=cache_add,
+xxx = 1
+base_path, morfeus_drive, train_generator, validation_generator = return_generators(batch_size=5, add=add,cache_add=cache_add,
                                                                                     flip=True, change_background=False,
                                                                                     threshold=True, threshold_val=10,
                                                                                     path_lead='Records', validation_name='',
-                                                                                    cache=False)
-layers_dict = get_layers_dict_dense_HNet(layers=2, filters=32, num_conv_blocks=4, conv_lambda=2)
-model_path = os.path.join(base_path, 'Keras', 'DenseNet', 'Models', 'Trial_ID_21', 'final_model.h5')
-model = return_model(layers_dict=layers_dict, densenet=True, all_trainable=True, weights_path=model_path)
-model.save(os.path.join(base_path, 'Keras', 'DenseNet', 'Models', 'Trial_ID_21', 'Model'))
-generator = iter(validation_generator.data_set)
-if not os.path.exists(os.path.join(base_path, 'Predictions_np')):
-    os.makedirs(os.path.join(base_path, 'Predictions_np'))
-for i in range(len(validation_generator)):
-    print(i)
-    x, y = next(generator)
-    pred = model.predict(x)
-    np.save(os.path.join(base_path, 'Predictions_np', 'Prediction_{}.npy'.format(i)), pred)
-    np.save(os.path.join(base_path, 'Predictions_np', 'Image_{}.npy'.format(i)), np.squeeze(x[0].numpy()))
-    np.save(os.path.join(base_path, 'Predictions_np', 'Truth_{}.npy'.format(i)), np.squeeze(y[0].numpy()))
-    xxx = 1
-# model.compile(tfk.optimizers.Adam(),
-#               loss=tfk.losses.SparseCategoricalCrossentropy(from_logits=False),
-#               metrics=[tfk.metrics.SparseCategoricalAccuracy(), SparseCategoricalMeanDSC(num_classes=2)])
-# model.fit(train_generator.data_set, epochs=10,steps_per_epoch=len(train_generator),
-#           validation_data=validation_generator.data_set, validation_steps=len(validation_generator),
-#           validation_freq=1)
+                                                                                    cache=False, wanted_keys={'inputs':['image','mask'],'outputs':['annotation']})
+model = return_model(layers_dict=None, is_2D=True, densenet=True, all_trainable=False)
+model.compile(tfk.optimizers.Adam(),
+              loss=tfk.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=[tfk.metrics.SparseCategoricalAccuracy(), SparseCategoricalMeanDSC(num_classes=2)])
+# model.load_weights(os.path.join('.', 'Test.hdf5'))
+# xx, yy = next(iter(validation_generator.data_set))
+model.fit(train_generator.data_set, epochs=10,steps_per_epoch=len(train_generator),
+          validation_data=validation_generator.data_set, validation_steps=len(validation_generator),
+          validation_freq=1)
+# model.save_weights(os.path.join('.', 'Test.hdf5'))
+# model.save(os.path.join('.', 'Test'), save_format='h5')
 # xxx = 1
 # model = DenseUNet(reduction=0.5)
 # model.load_weights(r'H:\Liver_Disease_Ablation\densenet161_weights_tf.h5', by_name=True)
