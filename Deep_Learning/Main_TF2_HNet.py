11b855e551416b626d7899141a1181f490431c47
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-10 12:07:47 -0500

    run with 3D on

--- Deep_Learning/Main_TF2_HNet.py
+++ Deep_Learning/Main_TF2_HNet.py
@@ -94,7 +94,7 @@ all_trainable = False
 weights_path = os.path.join(base_path, 'Keras', model_name, 'Models', 'Trial_ID_13', 'cp-0101.h5')
 
 
-find_dense_lr_densenet121_3D_pretrained = True
+find_dense_lr_densenet121_3D_pretrained = False
 if find_dense_lr_densenet121_3D_pretrained:
     from Optimization.Find_Best_LR_TF2_Dense import find_best_lr_DenseNet3D
     find_best_lr_DenseNet3D(batch_size=batch_size, path_desc=path_desc, add=add, cache_add=cache_add,
@@ -107,16 +107,16 @@ Plot the LR, get the min and max from the images
 plot_lr = False
 if plot_lr:
     from Optimization.Plot_Best_LR import make_plots
-    path = os.path.join(morfeus_drive,path_desc, 'DenseNet121')
+    path = os.path.join(morfeus_drive,path_desc, model_name)
     make_plots(path)
 
-run_200_retrained = False
+run_200_retrained = True
 if run_200_retrained:
     from Run_Model_TF2 import train_DenseNet3D
-    run_best = True
-    train_DenseNet3D(epochs=101, model_name=model_name, run_best=run_best, add=add,  cache_add=cache_add, batch_size=0,
+    run_best = False
+    train_DenseNet3D(epochs=101, model_name=model_name, run_best=run_best, add=add,  cache_add=cache_add, batch_size=batch_size,
                      change_background=False, path_lead='Records', validation_name='_64', all_trainable=all_trainable,
-                     weights_path=weights_path)
+                     weights_path=weights_path, excel_file_name=excel_file_name)
 
 
 '''
@@ -131,7 +131,7 @@ if run_200_retrained:
     run_best = True
     train_DenseNet3D(epochs=201, model_name=model_name, run_best=run_best, add=add,  cache_add=cache_add, batch_size=0,
                      change_background=False, path_lead='Records', validation_name='_64', all_trainable=all_trainable,
-                     weights_path=weights_path)
+                     weights_path=weights_path, excel_file_name=excel_file_name)
 make_opt_excel = False
 if make_opt_excel:
     '''
