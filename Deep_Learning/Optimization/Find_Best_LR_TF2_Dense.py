084a142e8beebd79a0c115f4eb449b9a0bd1cf0f
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-10 10:05:18 -0500

    added conv_blocks equal to 2, might as well make it divisible by 4

--- Deep_Learning/Optimization/Find_Best_LR_TF2_Dense.py
+++ Deep_Learning/Optimization/Find_Best_LR_TF2_Dense.py
@@ -97,7 +97,7 @@ def find_best_lr_DenseNet3D(batch_size=0, path_desc='', add='_16', cache_add='_1
     max_lr = 1
     for iteration in [0]:
         for layers in [2, 3]:
-            for num_conv_blocks in [4]:
+            for num_conv_blocks in [2, 4]:
                 for conv_lambda in [2, 4, 8]:
                     layers_dict = get_layers_dict_dense_HNet(layers=layers, filters=32, num_conv_blocks=num_conv_blocks, conv_lambda=conv_lambda,
                                                              max_conv_blocks=16)
