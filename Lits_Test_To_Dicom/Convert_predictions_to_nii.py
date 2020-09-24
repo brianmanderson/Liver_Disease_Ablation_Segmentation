193fa21eb237f31fab5729af391ff1a1b508fef8
markba122@gmail.com <bmanderson@mdanderson.org>
2020-09-24 17:00:31 -0500

    added RS to nii

--- Lits_Test_To_Dicom/Convert_predictions_to_nii.py
+++ Lits_Test_To_Dicom/Convert_predictions_to_nii.py
@@ -34,5 +34,47 @@ def mhd_to_nii():
     return None
 
 
+def RS_to_nii():
+    # Z, row, col
+    flip_dict = [0, 1, 2, 3, 4, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                 61, 62, 63, 64, 65, 66, 67, 68, 69]
+    base_path = r'H:\Liver_Disease_Ablation\LiTs_Test\Nifti'
+    pred_base_path = r'H:\Liver_Disease_Ablation\LiTs_Test\Dicom'
+    pred_files = [i for i in os.listdir(pred_base_path) if i.startswith('Lits_Test')]
+    reader = DicomReaderWriter(get_images_mask=True, arg_max=False,
+                               Contour_Names=['Liver_BMA_Program_4', 'Liver_Disease_Ablation_BMA_Program_0'])
+    for patient in pred_files:
+        index = int(patient.split('_')[-1])
+        pred_out_path = os.path.join(base_path, 'test-segmentation-{}.nii'.format(index))
+        print(patient)
+        if os.path.exists(pred_out_path):
+            continue
+
+        reader.Make_Contour_From_directory(os.path.join(pred_base_path, patient))
+        image_handle = sitk.ReadImage(os.path.join(base_path, 'test-volume-{}.nii'.format(index)))
+        mask = reader.mask
+        if index in flip_dict:
+            mask = mask[::-1]
+        prediction = mask[..., -1]
+        liver = mask[..., 1]
+        if np.max(liver) > 0:
+            prediction[liver == 0] = 0
+            prediction[prediction > 0] = 2
+            prediction[liver == 1] += 1
+            out_handle = sitk.GetImageFromArray(prediction)
+            out_handle.SetOrigin(image_handle.GetOrigin())
+            out_handle.SetDirection(image_handle.GetDirection())
+            out_handle.SetSpacing(image_handle.GetSpacing())
+            sitk.WriteImage(out_handle, pred_out_path)
+            prediction[liver == 1] -= 1
+            out_handle = sitk.GetImageFromArray(prediction)
+            out_handle.SetOrigin(image_handle.GetOrigin())
+            out_handle.SetDirection(image_handle.GetDirection())
+            out_handle.SetSpacing(image_handle.GetSpacing())
+            sitk.WriteImage(out_handle, pred_out_path)
+            xxx = 1
+    return None
+
+
 if __name__ == '__main__':
     pass
