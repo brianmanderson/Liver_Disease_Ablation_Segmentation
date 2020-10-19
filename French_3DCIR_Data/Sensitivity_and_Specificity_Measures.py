26f02dbbf80c9d5a9f9fe3a51d22ed2255582070
markba122@gmail.com <bmanderson@mdanderson.org>
2020-10-19 17:04:57 -0500

    update to use the modular sensitivyt and fp

--- French_3DCIR_Data/Sensitivity_and_Specificity_Measures.py
+++ French_3DCIR_Data/Sensitivity_and_Specificity_Measures.py
@@ -7,7 +7,67 @@ from Deep_Learning.Base_Deeplearning_Code.Plot_And_Scroll_Images.Plot_Scroll_Ima
 import os
 import pandas as pd
 from Segmentation_Evaluation_Tools.src.SegmentationEvaluationTools import determine_sensitivity,\
-    determine_false_positive_rate_and_false_volume
+    determine_false_positive_rate_and_false_volume, identify_overlap_metrics
+
+
+def single_site_comparison(path):
+    prediction_files = [i for i in os.listdir(path) if i.endswith('_Prediction.nii') and
+                        i.split('_Prediction')[0] not in ['Patient_5', 'Patient_11',
+                                                          'Patient_12', 'Patient_18', 'Patient_20']]
+    out_dict = {'Patient_Name': [], 'Volume (cc)': []}
+    Connected_Component_Filter = sitk.ConnectedComponentImageFilter()
+    Connected_Threshold = sitk.ConnectedThresholdImageFilter()
+    Connected_Threshold.SetLower(1)
+    Connected_Threshold.SetUpper(2)
+    truth_stats = sitk.LabelShapeStatisticsImageFilter()
+    pred_stats = sitk.LabelShapeStatisticsImageFilter()
+    for prediction_file in prediction_files:
+        patient_name = prediction_file.split('_Prediction')[0]
+        print(patient_name)
+        '''
+        First, load up the truth and prediction
+        '''
+        prediction_handle_base = sitk.ReadImage(os.path.join(path, prediction_file))
+        truth_handle_base = sitk.ReadImage(os.path.join(path, prediction_file.replace('_Prediction', '_Truth')),
+                                           sitk.sitkUInt8)
+        spacing = truth_handle_base.GetSpacing()
+        '''
+        Next, identify each independent segmentation in both
+        '''
+        truth_handle = Connected_Component_Filter.Execute(truth_handle_base)
+        prediction_handle = Connected_Component_Filter.Execute(prediction_handle_base)
+        '''
+        Get the independent lesions of both
+        '''
+        truth_stats.Execute(truth_handle)
+        pred_stats.Execute(prediction_handle)
+
+        prediction = sitk.GetArrayFromImage(prediction_handle_base)
+        for label in truth_stats.GetLabels():
+            truth_site = truth_handle == label
+            truth = sitk.GetArrayFromImage(truth_site)
+            overlap = truth * prediction
+            if np.max(overlap) == 0:  # If there is no overlap, take the closest one
+                truth_seed = prediction_handle_base.TransformPhysicalPointToIndex(truth_stats.GetCentroid(label))
+                pred_seeds = [pred_stats.GetCentroid(l) for l in pred_stats.GetLabels()]
+                pred_seeds = [prediction_handle_base.TransformPhysicalPointToIndex(i) for i in pred_seeds]
+                seeds = [pred_seeds[np.argmin(np.sqrt(np.sum(np.subtract(pred_seeds, truth_seed)**2, axis=-1)),
+                                              axis=-1)]]
+            else:
+                seeds = np.transpose(np.asarray(np.where(overlap > 0)))[..., ::-1]
+                seeds = [[int(i) for i in j] for j in seeds]
+            Connected_Threshold.SetSeedList(seeds)
+            grown_prediction = Connected_Threshold.Execute(prediction_handle_base)
+            volume = np.sum(truth) * np.prod(spacing) / 1000
+            tumor_data = identify_overlap_metrics(prediction_handle=grown_prediction, truth_handle=truth_site,
+                                                  perform_distance_measures=True)
+            out_dict['Patient_Name'].append(patient_name)
+            out_dict['Volume (cc)'].append(volume)
+            for key in tumor_data.keys():
+                if key not in out_dict:
+                    out_dict[key] = []
+                out_dict[key].append(tumor_data[key])
+    return out_dict
 
 
 def compile_sensitivity(path):
