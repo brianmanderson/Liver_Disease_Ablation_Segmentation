1c48efb6a4c1d4c32cfb1aa14739ef7ac4b5eb13
markba122@gmail.com <bmanderson@mdanderson.org>
2020-10-19 16:03:23 -0500

    export ablaion, GTV, prediction and liver contours if present as .mhd files

--- Raystation_Code/Export_mhds_Raystation.py
+++ Raystation_Code/Export_mhds_Raystation.py
@@ -5,9 +5,6 @@ from connect import *
 import os, time
 
 
-path = r'H:\Single_Site'
-for i in os.listdir(path):
-    os.remove(os.path.join(path, i))
 patient_db = get_current('PatientDB')
 patient = get_current('Patient')
 case = get_current('Case')
@@ -15,27 +12,14 @@ exam = get_current('Examination')
 rois_in_case = []
 for roi in case.PatientModel.RegionsOfInterest:
     rois_in_case.append(roi.Name)
-
-exported = False
+path = r'H:\Single_Site\Ablation'
+path = os.path.join(path, patient.PatientID, case.CaseName, exam.Name)
+if not os.path.exists(path):
+    os.makedirs(path)
 for roi in rois_in_case:
-    if roi in ['Ablation','GTV','Liver_Disease_Ablation_BMA_Program_0']:
+    if roi in ['Ablation','GTV','Liver_Disease_Ablation_BMA_Program_0', 'Liver', 'Liver_BMA_Program_4']: #
         if case.PatientModel.StructureSets[exam.Name].RoiGeometries[roi].HasContours():
             case.PatientModel.StructureSets[exam.Name].RoiGeometries[roi].ExportRoiGeometryAsMetaImage(
                 MetaFileName=os.path.join(path,'{}.mhd'.format(roi)), AsExamination=True)
-            exported = True
-
-if exported:
-    case.ScriptableDicomExport(ExportFolderPath=path, Examinations=[exam.Name], RtStructureSetsForExaminations=[])
-    fid = open(os.path.join(path,'Completed_Export.txt'),'w+')
-    fid.close()
-    while not os.path.exists(os.path.join(path, 'Completed.txt')):
-        print('Waiting...')
-        time.sleep(5)
-    pi = patient_db.QueryPatientsFromPath(Path=path, SearchCriterias={'PatientID': patient.PatientID})[0]
-    studies = patient_db.QueryStudiesFromPath(Path=path, SearchCriterias=pi)
-    series = []
-    for study in studies:
-        series += patient_db.QuerySeriesFromPath(Path=path, SearchCriterias=study)
-    patient.ImportDataFromPath(Path=path, CaseName=case.CaseName, SeriesOrInstances=series, AllowMismatchingPatientID=True)
-    for i in os.listdir(path):
-        os.remove(os.path.join(path,i))
+fid = open(os.path.join(path,'Completed_Export.txt'),'w+')
+fid.close()
