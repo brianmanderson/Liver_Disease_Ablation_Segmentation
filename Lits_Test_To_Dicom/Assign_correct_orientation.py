1103c0f114bb639f1ca88756d93f26f32753cd35
markba122@gmail.com <bmanderson@mdanderson.org>
2020-10-22 09:18:59 -0500

    old function to assing orientation

--- Lits_Test_To_Dicom/Assign_correct_orientation.py
+++ Lits_Test_To_Dicom/Assign_correct_orientation.py
@@ -6,15 +6,20 @@ from Pre_Processing.Dicom_RT_and_Images_to_Mask.src.DicomRTTool import DicomRead
 
 def assign_orientation():
     path = r'H:\Liver_Disease_Ablation\LiTs_Test\Dicom\Lits_Test_{}'
+    patient_dict = {}
+    # patient_dict[0] = 'FFS'
+    # patient_dict[53] = 'FFS'
+    patient_dict[57] = 'FFS'
+    patient_dict[59] = 'FFS'
     reader = DicomReaderWriter()
-    needs_adjustment = [5]
-    for patient in needs_adjustment:
+    for patient in patient_dict.keys():
         pat_path = path.format(patient)
+        print(pat_path)
         reader.down_folder(pat_path)
         files = [i for i in os.listdir(pat_path) if i.startswith('image') and i.endswith('.dcm')]
         for file in files:
             ds = pydicom.read_file(os.path.join(pat_path, file))
-            ds.PatientPosition = 'FFS'
+            ds.PatientPosition = patient_dict[patient]
             pydicom.write_file(os.path.join(pat_path, file), ds)
     return None
 
