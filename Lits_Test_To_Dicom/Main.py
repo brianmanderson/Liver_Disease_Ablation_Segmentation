06e932c71358b7bade9527bb5cc5abf8c6ec9b69
markba122@gmail.com <bmanderson@mdanderson.org>
2020-10-22 09:19:48 -0500

    added method to Main function for image acquistion parameters

--- Lits_Test_To_Dicom/Main.py
+++ Lits_Test_To_Dicom/Main.py
@@ -1,12 +1,18 @@
 __author__ = 'Brian M Anderson'
 # Created on 9/8/2020
 
+'''
+Obtain image parameters
+'''
+obtain_parameters = False
+if obtain_parameters:
+    from Obtain_Image_Acquisition_Parameters import obtain_image_parameters
+    obtain_image_parameters()
 
 '''
 First, convert the nii files to dicom
 '''
 convert_lits_to_dicom = False
-
 if convert_lits_to_dicom:
     from convert_lits_nii_to_dicom import *
     convert_nii_to_dicom()
@@ -39,14 +45,20 @@ Create disease contours
 #  Run Create_Disease_Contours
 
 '''
-Export predictions as .mhd files
+Export predictions as .mhd files or RS structures...
 '''
 #  Run Export_Predictions_as_mhd
-
+#  Run Export_Predictions_as_RS
 '''
 Convert Prediction .mhd files to .nii files for submission
 '''
-convert_mhd_to_nii = True
-if convert_mhd_to_nii:
-    from Convert_mhd_to_nii import mhd_to_nii
-    mhd_to_nii()
+predictions_to_nii = True
+if predictions_to_nii:
+    from Convert_predictions_to_nii import mhd_to_nii, RS_to_nii
+    # mhd_to_nii()
+    RS_to_nii()
+
+zip_predictions = False
+if zip_predictions:
+    from Zip_Results import zip_files
+    zip_files()
