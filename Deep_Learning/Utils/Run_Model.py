61a6c8c4c1618019e8920e9a65a94b64a17843f3
markba122@gmail.com <bmanderson@mdanderson.org>
2021-05-21 10:52:23 -0500

    image frequency from 1 to 5

--- Deep_Learning/Utils/Run_Model.py
+++ Deep_Learning/Utils/Run_Model.py
@@ -25,7 +25,7 @@ def run_model(model, train_generator, validation_generator, min_lr, max_lr, mode
     lrate = SGDRScheduler(min_lr=min_lr, max_lr=max_lr, steps_per_epoch=len(train_generator), cycle_length=step_factor,
                           lr_decay=0.5, mult_factor=1, gentle_start_epochs=0, gentle_fraction=1.0)
     add_lr = Add_Images_and_LR(log_dir=tensorboard_path, add_images=True, validation_data=validation_generator.data_set,
-                               number_of_images=len(validation_generator))
+                               number_of_images=len(validation_generator), image_frequency=5)
     early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=25, verbose=True, mode='max')
     callbacks = [tensorboard, lrate, add_lr]
     # if epochs < 9000:
